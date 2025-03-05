e3dd7dc73342ee3bc11c4a9d4052ad12bb29b536
nailtonmauricio <nailtonmauricio@hotmail.com>
2025-03-05 01:20:44 +0000

    Adicionado o recurso de tooltipes para as views, refatorando o arquivo app.js, package.json e package-lock.json

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,16 @@ import './bootstrap';
 import './scripts-admin';
 import './scripts-sbadmin';
 import './charts/failedLoginStatistics';
+import { MaskInput } from "maska";
+import { Tooltip } from 'bootstrap';
+import '@fortawesome/fontawesome-free/js/all.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Inicializa Maska para todos os inputs com o seletor data-maska
+    new MaskInput("[data-maska]");
+
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    tooltipTriggerList.forEach(tooltipTriggerEl => {
+        new Tooltip(tooltipTriggerEl);
+    });
+});
