cec281c1c6bbbb1dbcd85e0409ba9d889acbf752
Nailton M. Araujo <nailtonmauricio@hotmail.com>
2025-11-13 20:39:54 -0300

    Ajustes na view de login e inserção da imagem no diretório public/images

--- resources/views/modules/login/index.blade.php
+++ resources/views/modules/login/index.blade.php
@@ -7,14 +7,14 @@
         @csrf
         <div class="form-floating mb-3">
             <input type="email" class="form-control" id="email" name="email" placeholder="e-mail">
-            <label for="email">E-Mail</label>
+            <label for="email">E-Mail de usuário</label>
         </div>
         <div class="form-floating mb-3">
             <input type="password" class="form-control" id="password" name="password" placeholder="Senha">
             <label for="password">Senha</label>
         </div>
         <div class="row mt-3 mb-4">
-            <div class="col-6">
+            <div class="col-8">
                 <a href="{{ route('recover.index') }}" class="nav-link">
                     Esqueceu a senha?
                 </a>
--- resources/views/templates/login/index.blade.php
+++ resources/views/templates/login/index.blade.php
@@ -11,33 +11,68 @@
     ])
     <link rel="icon" type="image/png" href="{{ env('APP_ICON') }}"/>
     <style>
-        html, body {
-            height: 100%;
+
+
+        body {
+            background: url('{{ asset('images/bg-tech.png') }}') no-repeat center center fixed;
+            background-size: cover;
+            font-family: 'Inter', sans-serif;
+        }
+
+        .login-container {
+            min-height: 100vh;
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+            padding: 150px;
+            backdrop-filter: blur(4px);
+        }
+
+        .login-card {
+            background: rgba(255, 255, 255, 0.9);
+            border-radius: 16px;
+            padding: 40px;
+            max-width: 400px;
+            width: 100%;
+            box-shadow: 0 0 30px rgba(0, 0, 0, 0.3);
+        }
+
+        .login-card h3 {
+            font-weight: 600;
+            color: #222;
+        }
+
+        @media (max-width: 768px) {
+            .login-container {
+                justify-content: center;
+                padding: 20px !important;
+            }
+
+            .login-card {
+                max-width: 100%;
+                padding: 25px !important;
+            }
+
+            body {
+                background-attachment: scroll !important;
+                background-position: center !important;
+            }
         }
     </style>
 </head>
-<body class="h-100">
-<div class="container-fluid h-100">
-    <div class="row h-100">
-        <!-- Área de fundo oculta em dispositivos menores -->
-        <div class="col-md-9 d-none d-md-block" style="background-image: url('{{--{{ asset('images/bg-01.jpg') }}--}}'); background-size: cover; background-position: center;"></div>
+<body>
+<div class="login-container">
+    <div class="login-card">
+        <x-alert/>
+        @yield('content')
 
-        <!-- Área de formulário ajustável -->
-        <div class="col-12 col-md-3 p-5 mt-5">
-            <div class="row">
-                <x-alert/>
-                @yield('content')
-                <div class="row text-center">
-                    <div class="col-12">
-                        <p>conheça nossas redes</p>
-                    </div>
-                    <div class="col-12 d-flex justify-content-center gap-3">
-                        <a href="https://jitte.com.br" target="_blank"><i class="fa-solid fa-globe"></i></a>
-                        <a href="https://github.com/nailtonmauricio" target="_blank"><i class="fa-brands fa-github"></i></a>
-                        <a href="https://www.linkedin.com/in/nailtonmauricio" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
-                    </div>
-                </div>
-            </div>
+        <div class="col-12">
+            <p>conheça nossas redes</p>
+        </div>
+        <div class="col-12 d-flex justify-content-center gap-3">
+            <a href="{{ env('WEB_SITE') }}" target="_blank"><i class="fa-solid fa-globe"></i></a>
+            <a href="{{ env('GITHUB') }}" target="_blank"><i class="fa-brands fa-github"></i></a>
+            <a href="{{ env('LINKEDIN') }}" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
         </div>
     </div>
 </div>
